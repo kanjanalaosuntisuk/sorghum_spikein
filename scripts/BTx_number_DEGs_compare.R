7a50521dc66e8b5b0fc5add7251cb856b3c37f93
Kanjana Laosuntisuk <klaosun@ncsu.edu>
2024-01-28 16:08:09 -0500

    update script

--- scripts/BTx_number_DEGs_compare.R
+++ scripts/BTx_number_DEGs_compare.R
@@ -14,7 +14,7 @@ setwd("~/sorghum_spikein")
 
 #################################################################
 ##### import data --------------------------------------------------------------
-Sb_DEG <- read_excel("Input/sorghum_number of DEGs_new.xlsx")
+Sb_DEG <- read_excel("input/sorghum_number of DEGs_new.xlsx")
 Sb_DEG <- as.data.frame(Sb_DEG)
 Sb_DEG$norm_methods <- as.factor(Sb_DEG$norm_methods)
 Sb_DEG$norm_methods  <- factor(Sb_DEG$norm_methods, 
@@ -29,14 +29,6 @@ Sb_DEG %>%
   filter(norm_methods != "EdgeR_Nudelta") %>%
   dplyr::select(DE_tool, norm_methods, AM_up, PM_up) %>%
   gather(key = TOD, value = number, -DE_tool, -norm_methods) -> Sb_DEG_control
-
-ggplot(Sb_DEG_control, aes(x = norm_methods, y = number, fill = TOD)) + 
-  geom_bar(position="stack", stat="identity") +
-  coord_flip() +
-  xlab("DE Tools and Normalization Methods") +
-  ylab("Number of DEGs") +
-  labs(fill = "") +
-  theme_minimal()
 
 ### make a back-to-back barchart
 Sb_DEG_control$norm_methods <- factor(Sb_DEG_control$norm_methods, 
@@ -120,14 +112,6 @@ Sb_DEG %>%
   dplyr::select(DE_tool, norm_methods, AM_up, PM_up) %>%
   gather(key = TOD, value = number, -DE_tool, -norm_methods) -> Sb_DEG_cold
 
-ggplot(Sb_DEG_cold, aes(x = norm_methods, y = number, fill = TOD)) + 
-  geom_bar(position="stack", stat="identity") +
-  coord_flip() +
-  xlab("DE Tools and Normalization Methods") +
-  ylab("Number of DEGs") +
-  labs(fill = "") +
-  theme_minimal()
-
 ### make a back-to-back barchart -----
 Sb_DEG_cold$norm_methods <- factor(Sb_DEG_cold$norm_methods, 
                                       levels = rev(levels(Sb_DEG_cold$norm_methods)))
@@ -203,14 +187,6 @@ ggplot(data = Sb_DEG_cold_DESeq2_n,
 ggsave(filename = "cold_ampm_DEG_DESeq2_n.jpeg", plot = last_plot(),
        width = 5, height = 2, dpi = 300, units = "in", device='png')
 
-###################################################
-### combine figure 
-Sb_DEG_plot <- ggarrange(Sb_DEG_control_plot, Sb_DEG_cold_plot, 
-                           ncol = 2, common.legend = TRUE, legend = "bottom")
-# save plot
-ggsave(filename = "number_DEGs_plot.png", plot = Sb_DEG_plot, width = 12, height = 5, 
-       dpi = 300, units = "in", device='png')
-
 #################################################################
 ### select control_cold_am data ----
 Sb_DEG %>%
@@ -220,14 +196,6 @@ Sb_DEG %>%
   gather(key = TOD, value = number, -DE_tool, -norm_methods) -> Sb_DEG_ctrlcold_am
 Sb_DEG_ctrlcold_am$TOD[Sb_DEG_ctrlcold_am$TOD == "AM_up"] <- "control_up"
 Sb_DEG_ctrlcold_am$TOD[Sb_DEG_ctrlcold_am$TOD == "PM_up"] <- "chilling_up"
-
-ggplot(Sb_DEG_ctrlcold_am, aes(x = norm_methods, y = number, fill = TOD)) + 
-  geom_bar(position="stack", stat="identity") +
-  coord_flip() +
-  xlab("DE Tools and Normalization Methods") +
-  ylab("Number of DEGs") +
-  labs(fill = "") +
-  theme_minimal()
 
 ### make a back-to-back barchart ----
 Sb_DEG_ctrlcold_am$norm_methods <- factor(Sb_DEG_ctrlcold_am$norm_methods, 
@@ -326,14 +294,6 @@ Sb_DEG %>%
 Sb_DEG_ctrlcold_pm$TOD[Sb_DEG_ctrlcold_pm$TOD == "AM_up"] <- "control_up"
 Sb_DEG_ctrlcold_pm$TOD[Sb_DEG_ctrlcold_pm$TOD == "PM_up"] <- "chilling_up"
 
-ggplot(Sb_DEG_ctrlcold_pm, aes(x = norm_methods, y = number, fill = TOD)) + 
-  geom_bar(position="stack", stat="identity") +
-  coord_flip() +
-  xlab("DE Tools and Normalization Methods") +
-  ylab("Number of DEGs") +
-  labs(fill = "") +
-  theme_minimal()
-
 ### make a back-to-back barchart ----
 Sb_DEG_ctrlcold_pm$norm_methods <- factor(Sb_DEG_ctrlcold_pm$norm_methods, 
                                           levels = rev(levels(Sb_DEG_ctrlcold_pm$norm_methods)))
@@ -418,12 +378,4 @@ ggplot(data = Sb_DEG_ctrlcold_pm_DESeq2_n,
 # save plot
 ggsave(filename = "ctrl_cold_pm_DEG_DESeq2_n.jpeg", plot = last_plot(),
        width = 5, height = 2, dpi = 300, units = "in", device='png')
-
-###################################################
-### combine figure 
-Sb_DEG_plot2 <- ggarrange(Sb_DEG_ctrlcold_am_plot, Sb_DEG_ctrlcold_pm_plot, 
-                         ncol = 2, common.legend = TRUE, legend = "bottom")
-# save plot
-ggsave(filename = "number_DEGs_plot2.png", plot = Sb_DEG_plot2, width = 12, height = 5, 
-       dpi = 300, units = "in", device='png')
 
